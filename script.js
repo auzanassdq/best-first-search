cd488adf9e1f6bad35c52abd5300ef375f88a016
auzanassdq <auzanassidqi@gmail.com>
2019-11-13 20:19:52 +0700

    make it simple

--- script.js
+++ script.js
@@ -3,53 +3,64 @@ const Graph = require("./Graph")
 let graph = new Graph()
 graph.startState()
 
+let openArr = []
+let closeArr = []
+let searchStatus = false
+
+const bestFS = (from, to) => {
+
+  printResult(openArr, closeArr)
+  pos = graph.list.find(v => v.name == from)
+  to = graph.list.find(v => v.name == to)
+  
+  openArr.push(pos)
+  printResult(openArr, closeArr)
+
+  while (!searchStatus){
+    if (openArr[0].name != to.name){
+      if (openArr[0].direction.length != 0){
+
+        openArr[0].direction.forEach(item => openArr.push(item))
+        setVisited(closeArr, openArr)
+        openArr.sort(compareNumeric)
+        printResult(openArr, closeArr)
+
+      } else {
+        setVisited(closeArr, openArr)
+        printResult(openArr, closeArr)
+      }
+    } else {
+      setVisited(closeArr, openArr)
+      printResult(openArr, closeArr)
+      searchStatus = true
+    }
+  }
+  printResult(openArr, closeArr)
+}
+
+setVisited = (visited, unvisited) => {
+  visited.push(unvisited[0])
+  unvisited.splice(0, 1)
+}
+
 const compareNumeric = (a, b) => {
   if (a.cost > b.cost) return 1
   if (a.cost == b.cost) return 0
   if (a.cost < b.cost) return -1
 }
 
-const bestFS = (from, to) => {
-  let openArr = []
-  let closeArr = []
-  let searchStatus = false
-  pos = graph.list.find(v => v.name == from)
-  to = graph.list.find(v => v.name == to)
-  
-  openArr.push(pos)
-
-  while (!searchStatus){
-    if (openArr[0].name != to.name){
-      if (openArr[0].direction.length != 0){
-        
-        openArr[0].direction.map(item => openArr.push(item))
-        closeArr.push(openArr[0])
-        openArr.splice(0, 1)
-        openArr.sort(compareNumeric)
-
-      } else {
-        closeArr.push(openArr[0])
-        openArr.splice(0, 1)
-      }
-    } else {
-      closeArr.push(openArr[0])
-      openArr.splice(0, 1)
-      searchStatus = true
-    }
-  }
-  
-  let open = ""
-  openArr.forEach(item => {
-    open += item.name + " "
+getResult = (arr) => {
+  let res = ""
+  arr.forEach(item => {
+    res += item.name + " "
   })
-  console.log(open)
+  return res
+}
 
-  let close = ""
-  closeArr.forEach(item => {
-    close += item.name + " "
-  })
-  console.log(close)
-
+printResult = (openArr, closeArr) => {
+  console.log(`Unvisited : ${getResult(openArr)}`)
+  console.log(`Visited : ${getResult(closeArr)}`)
+  console.log("")
 }
 
 bestFS("S", "I")
